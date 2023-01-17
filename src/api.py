b00e4ae91b93988ef3cecc3cf71e3c0869f700e9
Rafael Trevisan <rafael.trevisan@inf.ufrgs.br>
2023-01-17 17:33:53 -0300

    added environment & remade calls

--- src/api.py
+++ src/api.py
@@ -1,13 +1,15 @@
 import requests
 import json
 
-def call_api_cluster(address):
-    response = requests.get("http://" + url + "/api/v5/clients")
+def call_api_cluster(address,port):
+    print("calling http://" + address + ":" + port +"/api/v5/clients")
+    response = requests.get("http://" + address + ":" + port +"/api/v5/clients")
     return response
 
-def call_api_nodes(address):
-    response = requests.get("http://" + url + "/api/v5/nodes")
+def call_api_nodes(address, port):
+    print("calling http://" + address + ":" + port + "/api/v5/nodes")
+    response = requests.get("http://" + address + ":" + port + "/api/v5/nodes")
     return response
 
-def call_scale_cluster(address):
+def call_scale_cluster(address,port):
     print("TODO")
--- src/main.py
+++ src/main.py
@@ -1,29 +1,36 @@
 import time
 import os
-from api import call_api
+from api import call_api_cluster
+from api import call_api_nodes
 from scale_logic import scale_cluster
 
 if __name__ == "__main__" :
 
     cluster_address = os.getenv("API_ADDRESS_CLUSTER")
+    cluster_port = os.getenv("CLUSTER_PORT")
+    kube_address = os.getenv("API_ADDRESS_KUBE")
+    kube_port= os.getenv("KUBE_PORT")
+    api_interval = os.getenv("API_INTERVAL")
     max_queue = os.getenv("MAX_QUEUE")
     max_inflight = os.getenv("MAX_INFLIGHT")
 
-    print("starting with cluster address: " + cluster_address)
+    print("starting with cluster address: " + cluster_address + ":" + cluster_port)
+    print("kubernetes api address set to: " + kube_address + ":" + kube_port)
     print("max queue set to: " + max_queue + " and max inflight set to: " + max_inflight)
 
     while(True):
-
-        response_cluster = call_api_cluster(cluster_address)
-        response_nodes = call_api_nodes(cluster_address)
+        response_cluster = call_api_cluster(cluster_address,cluster_port)
+        response_nodes = call_api_nodes(cluster_address,cluster_port)
 
         if (response_cluster.status_code == 200 and response_nodes.status_code == 200):
              print(response_cluster.json())
              print(response_nodes.json())
 
-             #scale_cluster(int(max_inflight), int(max_queue) , avg_inflight , avg_queue)
+            # scale_cluster(int(max_inflight), int(max_queue) , avg_inflight , avg_queue)
              time.sleep(5)
 
         else:
             print("error in api request " + str((response_cluster.status_code)) + " "  + str(response_nodes.status_code))
+            print("its alive")
             time.sleep(5)
+
